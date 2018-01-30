31d7c5f86d70762347fe58ad33d68c382dc28620
Боровик Михаил <@DESKTOP-CNS49BJ>
2018-01-31 00:24:01 +0300

    developer_1

---
+++ Project/OOP/Собеседование developer/Task_2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Собеседование_developer
+{
+    class Task_2
+    {
+        public struct S : IDisposable
+        {
+            private bool dispose;
+            public void Dispose()
+            {
+                dispose = true;
+            }
+            public bool GetDispose()
+            {
+                return dispose;
+            }
+        }
+
+        /// <summary>
+        /// Что будет выведено в следующем случае:
+        /// true, true
+        /// true, false
+        /// false, true
+        /// false, false +
+        /// </summary>
+        public void Process()
+        {
+            var s = new S();
+            using (s)
+            {
+                Console.WriteLine(s.GetDispose());
+            }
+            Console.WriteLine(s.GetDispose());
+        }
+    }
+}
---
+++ Project/OOP/Собеседование developer/Task_4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Собеседование_developer
+{
+    class Task_4
+    {
+        public void Process()
+        {
+            int i = 1;
+            object obj = i;
+            ++i;
+            Console.WriteLine(i); // 2
+            Console.WriteLine(obj); // 1
+            Console.WriteLine((short)obj); // Ошибка
+        }
+    }
+}

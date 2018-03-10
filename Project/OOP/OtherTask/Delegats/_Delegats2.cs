c27d8886b2b658d6d482c8b9c2ce63093d20ad49
Mico-LIT <redgame0@gmail.com>
2018-03-10 23:40:03 +0300

    Delegats

---
+++ Project/OOP/OtherTask/Delegats/_Delegats2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.OtherTask.Delegats2
+{
+    static public class Myclass2
+    {
+        static public string Method2(string str)
+        {
+            return ($"Hi {str}");
+        }
+    }
+
+    public delegate string Mydelegate2(string s);
+   public class _Delegats2
+    {
+        _Delegats2()
+        {
+            Mydelegate2 mydelegate2 = new Mydelegate2(Myclass2.Method2);
+
+            Console.WriteLine(mydelegate2.Invoke("Mico"));
+
+            Console.WriteLine(mydelegate2("Judi"));
+        }
+
+    }
+}

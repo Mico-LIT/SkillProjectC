c33f74db8687fb24149e46172f064fd31860c510
Михаил Боровик <it30@otlnal.ru>
2018-03-12 17:58:54 +0700

    Delegats prolongation2

---
+++ Project/OOP/OtherTask/Delegats/_Delegats7.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.OtherTask.Delegats7
+{
+    delegate MyDelegats3 MyDelegats(MyDelegats1 d1, MyDelegats2 d2);
+    delegate string MyDelegats1();
+    delegate string MyDelegats2();
+    delegate string MyDelegats3();
+
+    class _Delegats7
+    {
+        MyDelegats3 func (MyDelegats1 d1, MyDelegats2 d2)
+        {
+            return delegate { return d1.Invoke() + d2.Invoke(); };
+        }
+
+        string str1() { return "Hello"; }
+        string str2() { return "World"; }
+
+        public _Delegats7()
+        {
+            MyDelegats mydelegats = new MyDelegats(func);
+            MyDelegats3 mydelegats3 = mydelegats.Invoke(new MyDelegats1(str1),new MyDelegats2(str2));
+
+            Console.WriteLine(mydelegats3.Invoke());
+        }
+    }
+}

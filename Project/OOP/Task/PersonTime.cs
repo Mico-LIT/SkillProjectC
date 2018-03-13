b3faafbe51b49d3d73f5d78367e799bda0debfbb
Михаил Боровик <it30@otlnal.ru>
2018-03-13 16:57:55 +0700

    Фикс

--- Project/OOP/Task/PersonTime.cs
+++ Project/OOP/Task/PersonTime.cs
@@ -17,6 +17,6 @@ namespace OOP.Task
         public PersonTime(int id, string name, double rate) : base(id, name, rate)
         {
         }
-        public override double Salary { get => 20.8 * 8 * Rate; }
+        public override double Salary { get { return 20.8 * 8 * Rate; } }
     }
 }

4e90e42c262e8863ed442069fca9227b755e0722
Mico-LIT <redgame0@gmail.com>
2018-03-19 00:25:58 +0300

    Events

---
+++ Project/OOP/OtherTask/Events/Event_1.cs
@@ -0,0 +1,46 @@
+﻿using OOP.OtherTask.Delegats8;
+using System.Security.Cryptography.X509Certificates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.OtherTask.Events
+{
+    delegate void EventDelegats();
+
+    class MyClass
+    {
+        public event EventDelegats MyEvent;
+
+        public void InvokeEvent()
+        {
+            MyEvent.Invoke();
+        }
+    }
+
+    class Event_1
+    {
+        void Method1()
+        {
+            Console.WriteLine("1");
+        }
+
+       public void Method2()
+        {
+            Console.WriteLine("2");
+        }
+        
+        public Event_1()
+        {
+            MyClass myClass = new MyClass();
+            
+            myClass.MyEvent += new EventDelegats(Method1);
+            myClass.MyEvent += Method2;
+            myClass.MyEvent += delegate () { Console.WriteLine("33"); };
+
+            myClass.InvokeEvent();
+        }
+    }
+}

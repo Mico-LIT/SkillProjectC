664446eb99fc3450c55619b5c1effb22a263c5a5
Михаил Боровик <it30@otlnal.ru>
2018-03-26 17:47:53 +0700

    Delegats Action, Predicate and Func

---
+++ Project/OOP/OtherTask/Delegats/_Delegats9.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.OtherTask.Delegats9
+{
+    class _Delegats9
+    {
+        Action<int, int> DelegatAction;
+        Predicate<int> DelegatPredic;
+        Func<int, double> DelegatFunc;
+
+        public _Delegats9()
+        {
+            DelegatAction = Method;
+            DelegatAction.Invoke(1, 2);
+
+            DelegatPredic = delegate (int t) { return t > 0; };
+            Console.WriteLine(DelegatPredic(1));
+
+            DelegatFunc = Method1;
+            DelegatFunc(2);
+
+            Console.ReadLine();
+        }
+
+        void Method(int t,int t1)
+        {
+            Console.WriteLine($"{t+t1}");
+        }
+
+        double Method1(int t)
+        {
+            double ret = t * 1.23;
+            Console.WriteLine(ret);
+            return ret;
+        }
+    }
+}
--- Project/OOP/Program.cs
+++ Project/OOP/Program.cs
@@ -11,7 +11,6 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            OOP.OtherTask.Delegats.Example.Unit_1 sd = new OtherTask.Delegats.Example.Unit_1();
         }
     }
 }

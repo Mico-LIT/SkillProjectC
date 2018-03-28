c3c05c4b65692d2926e4f210e2732849e5bd8cac
Михаил Боровик <it30@otlnal.ru>
2018-03-28 15:54:18 +0700

    Thread prolongation

---
+++ Project/OOP/OtherTask/Thread/Thread2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace OOP.OtherTask.Thread2
+{
+    class Thread2
+    {
+        public Thread2()
+        {
+            Thread thread = new Thread(new ThreadStart(Count));
+            thread.Start();
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("Thread 1");
+                Thread.Sleep(300);
+            }
+        }
+
+        void Count()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(new string(' ',10)+"Thread 2");
+                Thread.Sleep(400);
+            }
+        }
+    }
+}
---
+++ Project/OOP/OtherTask/Thread/Thread3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace OOP.OtherTask.Thread3
+{
+    class Thread3
+    {
+        public Thread3()
+        {
+            Thread thread = new Thread(new ParameterizedThreadStart(method));
+            thread.Start('2');
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("1");
+                Thread.Sleep(300);
+            }
+        }
+
+        void method(object s)
+        {
+            char ch = (char)s;
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(new string(ch, 20));
+                Thread.Sleep(400);
+            }
+        }
+    }
+}

f7cede974d1f844b3354c7d0f02c4a7f51e39c5e
Михаил Боровик <it30@otlnal.ru>
2018-04-03 17:25:27 +0700

    Thread prolongation3

---
+++ Project/OOP/OtherTask/Thread/Thread7.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OOP.OtherTask.Thread7
+{
+    class Thread7
+    {
+        object obj = new object();
+
+        void Method()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                lock (obj)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(new string(' ',20) + "Secondaty");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+            }
+        }
+
+        public Thread7()
+        {
+            Thread task = new Thread(Method);
+            task.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                lock (obj)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Primary");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+            }
+        }
+    }
+}

6b44f974c78a454bd6946e603b96377cda53a331
Михаил Боровик <it30@otlnal.ru>
2018-04-05 15:13:31 +0700

    Collections Prolongation

---
+++ Project/OOP/OtherTask/Collections/Collections2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.OtherTask.Collections
+{
+    class Collections2
+    {
+        public Collections2()
+        {
+            //Dequeue: извлекает и возвращает первый элемент очереди
+            //Enqueue: добавляет элемент в конец очереди
+            //Peek: просто возвращает первый элемент из начала очереди без его удаления
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            
+            Console.WriteLine(queue.Peek());
+            Console.WriteLine(queue.Dequeue());
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Project/OOP/OtherTask/Collections/Collections3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.OtherTask.Collections
+{
+    class Collections3
+    {
+        public Collections3()
+        {
+            //Push: добавляет элемент в стек на первое место
+            //Pop: извлекает и возвращает первый элемент из стека
+            //Peek: просто возвращает первый элемент из стека без его удаления
+
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(new string('_',20));
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

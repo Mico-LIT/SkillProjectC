9b3a85295b2ca0a162e0a2c78db9dc812c48e8ec
Михаил Боровик <it30@otlnal.ru>
2018-04-10 18:04:18 +0700

    Game is Exception

---
+++ Project/OOP/OtherTask/Game/CarBody.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace OOP.OtherTask.Game
+{
+    class CarBody
+    {
+        int left;
+        int top;
+
+        public CarBody(int left, int top)
+        {
+            this.left = left;
+            this.top = top;
+        }
+
+        public void Draw()
+        {
+            Thread th = new Thread(DrawCarBody);
+            th.Start();
+        }
+
+        void DrawCarBody()
+        {
+            Console.SetCursorPosition(left, top);
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(" ");
+                }
+                top++;
+                Console.SetCursorPosition(left,top);
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+
+    }
+}
---
+++ Project/OOP/OtherTask/Game/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.OtherTask.Game
+{
+    class Game
+    {
+        public Game()
+        {
+            Console.SetWindowSize(100, 50);
+
+            Console.CursorVisible = true;
+
+            CarBody a = new CarBody(44,15);
+            a.Draw();
+
+            Console.ReadLine();
+        }
+    }
+}

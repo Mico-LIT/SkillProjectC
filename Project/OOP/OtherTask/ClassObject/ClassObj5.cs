259ad3f9c0883beaf1485666e29a6184eae1810a
Михаил Боровик <it30@otlnal.ru>
2018-04-24 17:19:28 +0700

    Доливка Class obj

--- Project/OOP/OtherTask/ClassObject/ClassObj5.cs
+++ Project/OOP/OtherTask/ClassObject/ClassObj5.cs
@@ -6,12 +6,43 @@ using System.Threading.Tasks;
 
 namespace OOP.OtherTask.ClassObject
 {
-    class ClassObj5
+
+    // Клонирование глубокое. граф
+    //class A { public int a = 1; }
+    //class B : A { public int b = 2; }
+    //class C : B { public int c = 3;  }
+    //class X : C { }
+
+    // Клонирование ассоциации происходит поверхностно.
+    class A { public int a = 1; }
+    class B { public int b = 2; }
+    class C { public B B = new B(); }
+
+    class X
     {
+        public A A = new A();
+        public C C = new C();
+    }
+
+    class ClassObj5 : X
+    {
+
         public ClassObj5()
         {
-            Object obj = new Object();
-            Object obj2 = new Object();
+            ClassObj5 prog = new ClassObj5();
+
+            Console.WriteLine($" Original {prog.A.a} {prog.C.B.b}");
+
+            ClassObj5 clone = prog.MemberwiseClone() as ClassObj5;
+            Console.WriteLine($" Clone {prog.A.a} {prog.C.B.b}");
+
+            // Проверка на глубокое клонирование (Поверхностное клонирование)
+            clone.A.a = clone.C.B.b = 7;
+
+            Console.WriteLine($" Original {prog.A.a} {prog.C.B.b}");
+            Console.WriteLine($" Clone {prog.A.a} {prog.C.B.b}");
+
+            Console.ReadLine();
         }
     }
 }

290023cb9b77caf0071f181eef229c0542982ac3
Михаил Боровик <it30@otlnal.ru>
2018-05-11 18:06:40 +0700

    Linq prolongation

--- Project/OOP/OtherTask/LINQ/Linq_4.cs
+++ Project/OOP/OtherTask/LINQ/Linq_4.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OOP.OtherTask.LINQ
+namespace OOP.OtherTask.LINQ_
 {
     static class Myset
     {
---
+++ Project/OOP/OtherTask/LINQ/Linq_9.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.OtherTask.LINQ
+{
+    class Linq_9
+    {
+        class Result
+        {
+            public int Input { get; set; }
+            public int Output { get; set; }
+            public Result(int input, int output)
+            {
+                this.Input = input;
+                this.Output = output;
+            }
+        }
+
+        public Linq_9()
+        {
+
+            int[] numbers = { 1, 2, 3, 4 };
+
+            var query = from x in numbers
+                        select new Result(x, x * 2);
+
+            //var query = from x in numbers
+            //             select new { Output = x, Input = x * 2 };
+
+            numbers[0] = 777;
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}

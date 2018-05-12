8c38bac8ae2a2a157750f8f540e05a6af16b3219
Mico-LIT <redgame0@gmail.com>
2018-05-13 01:36:44 +0300

    Linq end

---
+++ Project/OOP/OtherTask/LINQ/LInq_13.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.OtherTask.LINQ
+{
+    class Linq_13
+    {
+        public Linq_13()
+        {
+            int[] i = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var query = from x in i
+                        group x by x % 4;
+
+            foreach (var item in query)
+            {
+                Console.WriteLine($"MOD {item.Key}");
+
+                foreach (var item2 in item)
+                {
+                    Console.WriteLine(item2);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Project/OOP/OtherTask/LINQ/Linq_15.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// into подобно let
+/// </summary>
+namespace OOP.OtherTask.LINQ
+{
+    class Linq_15
+    {
+        public Linq_15()
+        {
+            int[] i = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var query = from x in i
+                        group x by x % 2 into tmp
+                        where tmp.Key == 0
+                        select new { tmp.Key, Count=tmp.Count(), Group=tmp };
+
+            //var query = from x in i
+            //            group x by x % 2 into tmp
+            //            select new { tmp.Key, Count = tmp.Count(), Group = tmp };
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Count);
+                Console.WriteLine(item.Key);
+                foreach (var item2 in item.Group) Console.WriteLine(item);
+            }
+        }
+    }
+}

25e13577d49ca88a810ea4d39fd00e137e7e210a
Михаил Боровик <it30@otlnal.ru>
2018-05-25 18:14:56 +0700

    prolongation sql ddl

--- Project/SQL/SQL Essential/DDL/SQL_CheckConstraints.sql
+++ Project/SQL/SQL Essential/DDL/SQL_CheckConstraints.sql
@@ -1,40 +1,160 @@
 
 USE [C:\REPOSITORIES\SKILL\SKILLPROJECTC\PROJECT\SQL\SKILLDB.MDF] 
 
-DROP TABLE Customers
+-------------------------------------------------------------------------
+					 /*Пользовательские ограничения*/
+-------------------------------------------------------------------------
+	
+
+  DROP TABLE Customers
+  GO
   DROP TABLE Orders
-
-CREATE TABLE Customers(
-	CustomerNO int IDENTITY NOT NULL, 
-	CustomerName varchar(25) NOT NULL,
-	Address1 varchar(25) NOT NULL,
-	City     varchar(15) NOT NULL,
-	Contact  varchar(25) NOT NULL,
-	Phone char(12)    
-  )
-
-
--- Создание пользовательского ограничения на существующей таблице	
-
-ALTER TABLE Customers ADD CONSTRAINT CH_CustPhoneNo
-  -- Ограничение CHECK
-  CHECK (Phone LIKE '([0-9][0-9][0-9])[0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
   GO
 
-INSERT INTO Customers
-(CustomerName, Address1, City, Contact, Phone)
-VALUES
-('Alex', 'NewSTR', 'City', 'dfgjs@mail.ru', '(093)1231212');
 
-SELECT * FROM Customers
+ CREATE TABLE Customers(
+  CustomerNO INT NOT NULL IDENTITY,
+  CustomerName VARCHAR(30) NOT NULL,
+  Address1 VARCHAR(30) NOT NULL,
+  City VARCHAR(30) NOT NULL,
+  Contact VARCHAR(30) NOT NULL,
+  -- Пользовательское ограничение	     
+  Phone CHAR(12) CHECK(Phone LIKE('([0-9][0-9][0-9])[0-9][0-9][0-9][0-9][0-9][0-9][0-9]'))
+  )
+  GO
 
-INSERT INTO Customers
-(CustomerName, Address1, City, Contact, Phone)
-VALUES
-('Alex', 'NewSTR', 'City', 'dfgjs@mail.ru', '(093)*******'); -- Ошибка (Не соответствие шаблону).
+  INSERT INTO Customers																			   
+  (CustomerName, Address1, City, Contact, Phone)
+  VALUES
+  ('Alex', 'NewSTR', 'City', 'dfgjs@mail.ru', '(093)1231212');
 
--------------------------------------------------------------------------
+  INSERT INTO Customers
+  (CustomerName, Address1, City, Contact, Phone)
+  VALUES
+  ('Alex', 'NewSTR', 'City', 'dfgjs@mail.ru', '(093)*******'); -- Ошибка.
 
+  SELECT * FROM Customers;
+
+  DROP TABLE Customers;
+  GO
+
+  CREATE TABLE Customers(
+    CustomerNO INT IDENTITY NOT NULL,
+    CustomerName VARCHAR(30) NOT NULL,
+    Address1 VARCHAR(30) NOT NULL,
+    City VARCHAR(30) not NULL,
+    Contact VARCHAR(30) NOT NULL,
+    Phone CHAR(12) UNIQUE -- UNIQUE (алтернативный ключ), значение в ячейке должно быть уникальное
+    )
+  GO
+  
+  INSERT INTO Customers
+  (CustomerName, Address1, City, Contact, Phone)
+  VALUES
+  ('Alex', 'NewSTR', 'City', 'dfgjs@mail.ru', '(093)*******');
+  
+  
+  INSERT INTO Customers
+  (CustomerName, Address1, City, Contact, Phone)
+  VALUES
+  ('Alex', 'NewSTR', 'City', 'dfgjs@mail.ru', '(093)1231213'); 
+  
+  
+  SELECT * FROM Customers;
+  DROP TABLE Customers;
+
+  CREATE TABLE Customers                
+    (                                      
+  	CustomerNO int IDENTITY NOT NULL, 
+  	CustomerName varchar(25) NOT NULL,
+  	Address1 varchar(25) NOT NULL,
+  	City     varchar(15) NOT NULL,
+  	Contact  varchar(25) NOT NULL,
+    Phone    CHAR(12) UNIQUE CHECK(Phone LIKE('([0-9][0-9][0-9])[0-9][0-9][0-9][0-9][0-9][0-9][0-9]'))
+    )
+
+
+
+  INSERT INTO Customers
+  (CustomerName, Address1, City, Contact, Phone)
+  VALUES
+  ('Alex', 'NewSTR', 'City', 'dfgjs@mail.ru', '(093)1231212');
+  
+  INSERT INTO Customers
+  (CustomerName, Address1, City, Contact, Phone)
+  VALUES
+  ('Alex', 'NewSTR', 'City', 'dfgjs@mail.ru', '(093)*******'); -- Ошибка (Не соответствие шаблону).
+  
+  INSERT INTO Customers
+  (CustomerName, Address1, City, Contact, Phone)
+  VALUES
+  ('Alex', 'NewSTR', 'City', 'dfgjs@mail.ru', '(093)1231212'); -- Ошибка (Повторяющееся значение).
+  
+  SELECT * FROM Customers;
+
+
+  DROP TABLE Customers;
+  GO
+  
+  CREATE TABLE Customers                
+  (                                      
+  	CustomerNO int IDENTITY NOT NULL, 
+  	CustomerName varchar(25) NOT NULL,
+  	Address1 varchar(25) NOT NULL,
+  	City     varchar(15) NOT NULL,
+  	Contact  varchar(25) NOT NULL,
+  	Phone char(12)             
+  )
+  GO
+
+  -- Создание пользовательского ограничения на существующей таблице	
+    ALTER TABLE Customers ADD CONSTRAINT CN_CustomersPhoneNo
+  -- Ограничение
+    CHECK(Phone LIKE('([0-9][0-9][0-9])[0-9][0-9][0-9][0-9][0-9][0-9][0-9]'))
+    GO
+
+    INSERT INTO Customers
+    (CustomerName, Address1, City, Contact, Phone)
+    VALUES
+    ('Alex', 'NewSTR', 'City', 'dfgjs@mail.ru', '(093)1231212');
+    
+    INSERT INTO Customers
+    (CustomerName, Address1, City, Contact, Phone)
+    VALUES
+    ('Alex', 'NewSTR', 'City', 'dfgjs@mail.ru', '(093)*******'); -- Ошибка (Не соответствие шаблону).
+  
+-----------------------------------------------------
+    ALTER TABLE Customers
+    NOCHECK CONSTRAINT CN_CustomersPhoneNo  -- Отключение работы ограничения CHECK
+
+
+    INSERT INTO Customers
+    (CustomerName, Address1, City, Contact, Phone)
+    VALUES
+    ('Alex', 'NewSTR', 'City', 'dfgjs@mail.ru', '(093)*******');
+-----------------------------------------------------
+
+    ALTER TABLE Customers CHECK CONSTRAINT CN_CustomersPhoneNo --  Включение работы ограничения CHECK
+
+      
+    INSERT INTO Customers
+    (CustomerName, Address1, City, Contact, Phone)
+    VALUES
+    ('Alex', 'NewSTR', 'City', 'dfgjs@mail.ru', '(093)*******'); -- Ошибка (Не соответствие шаблону).
+
+-------------------------------------------------------
+    ALTER TABLE Customers
+    DROP CONSTRAINT	CN_CustomersPhoneNo;		--  Удаление ограничения CHECK
+    
+    INSERT INTO Customers
+    (CustomerName, Address1, City, Contact, Phone)
+    VALUES
+    ('Alex', 'NewSTR', 'City', 'dfgjs@mail.ru', '(093)*******');
+    
+    
+    --  После удаления не возможен возврат ограничения CHECK
+    ALTER TABLE Customers
+    CHECK CONSTRAINT CN_CustomersPhoneNo;	
 
 
 

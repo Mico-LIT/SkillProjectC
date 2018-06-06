944ca60d85b82fa2fdcdf862dc805ce11c600d73
Михаил Боровик <it30@otlnal.ru>
2018-06-06 16:25:31 +0700

    Normalization

--- Project/SQL/SQL Essential/Design/Normalization.sql
+++ Project/SQL/SQL Essential/Design/Normalization.sql
@@ -278,8 +278,64 @@ SELECT * FROM Orders
 SELECT * FROM OrderDetails
 SELECT * FROM Products
 
+-------------------------------------------------------------------------
+--                            Третья НФ
+-------------------------------------------------------------------------
 
-  
+-- Третья нормальная форма (3NF) – удовлетворяет 2NF, и ни в одном не 
+-- ключевом столбце не может быть зависимости от другого не ключевого 
+-- столбца. Наличие в таблице производных данных не допускается.
+
+DROP TABLE OrderDetails
+DROP TABLE Products
+
+CREATE TABLE Products
+(
+    ProductID char(5) NOT NULL PRIMARY KEY,
+    [Description] varchar(15),
+    UnitPrice smallmoney NOT NULL,
+    [Weight] numeric(18, 0) NULL
+)
+
+
+CREATE TABLE OrderDetails
+(
+    OrderID int NOT NULL,
+    LineItem int NOT NULL,
+    ProductID char(5) NOT NULL,
+    Qty int NOT NULL,
+
+    FOREIGN KEY(OrderID) REFERENCES Orders(OrderID),
+    FOREIGN KEY(ProductID) REFERENCES Products(ProductID),
+    PRIMARY KEY (OrderId, LineItem)
+)
+
+INSERT Products
+VALUES
+( 'LV231', 'Джинсы', 45, 2),
+( 'DG30', 'Ремень', 30,  1),
+( 'GC111', 'Футболка', 20,  2),
+( 'LV12', 'Обувь', 26,  2),
+( 'GC11', 'Шапка', 32,  1)
+
+
+INSERT OrderDetails
+VALUES
+( 1, 1, 'LV231', 5 ),
+( 1, 2, 'DG30', 5 ),
+( 1, 3, 'LV12', 5 ),
+( 2, 1, 'GC11', 10 ),
+( 2, 2, 'GC111', 15 ),
+( 3, 1, 'LV12', 20 ),
+( 3, 2, 'GC11', 18 )
+
+
+SELECT * FROM Customers
+SELECT * FROM Employees
+SELECT * FROM Orders
+SELECT * FROM OrderDetails
+SELECT * FROM Products
+
 -------------------------------------------------------------------------
 --                            Денормализация
 -------------------------------------------------------------------------

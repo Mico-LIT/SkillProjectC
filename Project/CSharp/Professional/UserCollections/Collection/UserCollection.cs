5887310fa9e2f9f21e97e622da1ff8cd059803c9
Михаил Боровик <it30@otlnal.ru>
2018-06-29 18:07:47 +0700

    C# Professional UserCollection

---
+++ Project/CSharp/Professional/UserCollections/Collection/UserCollection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace CSharp.Professional.UserCollections.Collection
+{
+    class UserCollection : IEnumerable, IEnumerator
+    {
+        readonly Element[] element = new Element[4];
+
+        int position = -1;
+
+        public Element this[int index]
+        {
+            get { return element[index]; }
+            set { element[index] = value; }
+        }
+
+        // Реализачия интерфейса IEnumerator
+        object IEnumerator.Current
+        {
+            get
+            {
+                return element[position];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)this;
+        }
+
+        bool IEnumerator.MoveNext()
+        {
+            if (position < element.Length -1)
+            {
+                position++;
+                return true;
+            }
+            ((IEnumerator)this).Reset();
+            return false;
+        }
+
+        void IEnumerator.Reset()
+        {
+            position = -1;
+        }
+    }
+}
---
+++ Project/CSharp/Professional/UserCollections/Collection2/Prog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Professional.UserCollections.Collection2
+{
+    class Prog
+    {
+        public Prog()
+        {
+            UserCollection<Element> coll = new UserCollection<Element>();
+            coll[0] = new Element(1, 2);
+            coll[1] = new Element(2, 3);
+            coll[2] = new Element(3, 4);
+            coll[3] = new Element(5, 6);
+
+            foreach (Element item in coll)
+            {
+                Console.WriteLine($"{item.FildA} {item.FildB}");
+            }
+
+            // Так работает foreach
+
+            IEnumerator enumer = (coll as IEnumerable).GetEnumerator();
+
+            while (enumer.MoveNext())
+            {
+                Element item = enumer.Current as Element;
+                Console.WriteLine($"{item.FildA} {item.FildB}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

d3d071f8b1aacad2b991fdd8c0c2690e6b8c132e
Михаил Боровик <it30@otlnal.ru>
2018-07-04 18:05:23 +0700

    Start TextWorking

---
+++ Project/CSharp/Professional/TextWorking/_String/_StringExamle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Professional.TextWorking._String
+{
+    class _StringExamle
+    {
+        public _StringExamle()
+        {
+            String s = "Hello";
+            Console.WriteLine(s);
+            Console.WriteLine(s.GetHashCode());
+            s = s.Insert(2, "mm");
+            //s[2]='m';
+            Console.WriteLine(s);
+            Console.WriteLine(GetHashCode());
+            ///
+            string s2 = new string('-', 20);
+            ///
+            s += s2;
+            ///
+            string s4 = 5.ToString();
+            //
+            string s5 = String.Format("{0} + {1} = {2}", 1, 2, 1 + 2);
+
+            Console.ReadLine();
+        }
+    }
+}

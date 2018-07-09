6b6125f53fe604b1bd7ebf323796b0b73ce4a604
Mico-LIT <redgame0@gmail.com>
2018-07-09 08:36:10 +0300

    Type

---
+++ Project/CSharp/Professional/Reflection/_Type/TypeTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Professional.Reflection._Type
+{
+    class MyClass
+    {
+    }
+
+    class TypeTest
+    {
+        public TypeTest()
+        {
+            //1
+            Type type;
+            MyClass myClass = new MyClass();
+            type = myClass.GetType();
+            Console.WriteLine("1 Way " + type);
+            //2
+            type = Type.GetType("CSharp.Professional.Reflection._Type.MyClass");
+            Console.WriteLine("2 way "+ type);
+            //3
+            type = typeof(MyClass);
+            Console.WriteLine("3 way "+ type);
+
+            Console.ReadLine();
+        }
+    }
+}

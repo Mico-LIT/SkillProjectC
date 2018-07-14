e741cecb2724c38e32cb02182f798179e92afeac
Mico-LIT <redgame0@gmail.com>
2018-07-14 23:15:35 +0300

    Serealizations start

---
+++ Project/CSharp/Professional/Serealizations/XML/Go.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace CSharp.Professional.Serealizations.XML
+{
+    class Go
+    {
+        readonly XmlSerializer serializer = new XmlSerializer(typeof(MyClassData));
+        MyClassData instance1 = new MyClassData();
+        MyClassData instance2;
+
+        public Go()
+        {
+            _Serialize();
+            _Deserialize();
+        }
+
+        private void _Deserialize()
+        {
+            FileStream stream = new FileStream("Test.xml",FileMode.Open,FileAccess.Read);
+            instance2 = serializer.Deserialize(stream) as MyClassData;
+            stream.Close();
+            Console.WriteLine($"{instance2.I}  {instance2.Str}");
+
+            foreach (var item in instance2.MyList)
+            {
+                Console.WriteLine($"{item}");
+            }
+
+            Console.ReadLine();
+        }
+
+        void _Serialize(){
+
+            for (int i = 0; i < 10; i++)
+            {
+                instance1.MyList.Add("Element: " + i);
+            }
+
+            FileStream fileStream = new FileStream("Test.xml", FileMode.Create, FileAccess.Write);
+            serializer.Serialize(fileStream, instance1);
+            Console.WriteLine("Serealization xml");
+            fileStream.Close();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Project/CSharp/Professional/Serealizations/XML/MyClassData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Xml.Serialization;
+
+namespace CSharp.Professional.Serealizations.XML
+{
+    [XmlRoot("root")]
+    public class MyClassData
+    {
+        private string str = "s1";
+        List<string> items = new List<string>();
+
+        [XmlAttribute("WorkStr")]
+        public string Str { get { return str; } set { str = value; } }
+
+        [XmlAttribute("int")]
+        public int I { get; set; } = 1;
+
+        [XmlElement("Pos")]
+        public Point Position { get; set; } = new Point(12, 15);
+
+        [XmlIgnore]
+        public string Password { get; set; } = "1234567890";
+
+        [XmlArray("Lists")]
+        [XmlArrayItem("Item")]
+        public List<string> MyList { get { return items; } set { items = value; } }
+    }
+}

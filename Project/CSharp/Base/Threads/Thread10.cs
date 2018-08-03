cccbde8815be03ca7dab46af7c3ae5fae88b3f26
Михаил Боровик <it30@otlnal.ru>
2018-08-03 11:38:46 +0700

    Thread prolongation

---
+++ Project/CSharp/Base/Threads/Thread10.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CSharp.Base.Threads.Thread10
+{
+    class Thread10
+    {
+        // Счетчик потоков
+        static long counter;
+        static object block = new object();
+
+        public void Procedure()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                //Interlocked.Increment(ref counter);
+
+                lock (block)
+                {
+                    counter++;
+                }
+            }
+        }
+
+        public Thread10()
+        {
+            Console.WriteLine("Ожидаемое значение 1000000");
+            Thread thread = new Thread(Procedure);
+            thread.Start();
+            Thread.Sleep(10);
+            Console.WriteLine("Реальное " + counter);
+            Console.Read();
+        }
+    }
+}
---
+++ Project/CSharp/Base/Threads/Thread9.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharp.Base.Threads.Thread9
+{
+    class Thread9
+    {
+        void Procedure()
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                Thread.Sleep(10);
+                Console.Write('.');
+            }
+            Console.WriteLine("\n Завершение вторичного потока");
+        }
+
+        public Thread9()
+        {
+            var thread = new Thread(Procedure);
+
+            // IsBackground - устанавливает поток как фоновый. не ждет завершения вторичного потока
+            // IsBackground по умолчанию = false
+            // thread.IsBackground = true;
+            thread.Start();
+            Thread.Sleep(500);
+
+            Console.WriteLine("\n Завершение главного потока");
+            Console.Read();
+        }
+    }
+}

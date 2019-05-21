4020b2daf19b8c437c02c963d56436b72efa57b2
Mico-LIT <redgame0@gmail.com>
2019-05-21 18:40:56 +0700

    The order in project

---
+++ Project/SQL/SQL Essential/Transactions_Triggers/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL.SQL_Essential.Transactions_Triggers
+{
+    class Class1
+    {
+        // TODO SQL Transactions..
+    }
+}

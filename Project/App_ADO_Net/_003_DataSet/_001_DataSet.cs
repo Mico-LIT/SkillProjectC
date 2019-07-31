d3e5f5802a9f000f6d305d5077c63a81d0882529
Mico-LIT <redgame0@gmail.com>
2019-07-31 18:16:18 +0700

    ADO.NET Step 2

---
+++ Project/App_ADO_Net/_003_DataSet/_001_DataSet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace App_ADO_Net._003_DataSet
+{
+    public class _001_DataSet
+    {
+        public _001_DataSet()
+        {
+            DataSet ds = new DataSet();
+
+            DataTable parentTable = new DataTable();
+            DataTable childTable = new DataTable();
+
+            DataColumn childColumn = childTable.Columns.Add("ChildColumn", typeof(int));
+            DataColumn parentColumn = parentTable.Columns.Add("ParentColumn", typeof(int));
+
+            ds.Tables.AddRange(new DataTable[] { childTable, parentTable });
+
+            parentTable.Constraints.Add(new UniqueConstraint(parentColumn));
+            childTable.Constraints.Add(new ForeignKeyConstraint(parentColumn, childColumn));
+
+            DataRow parentRow = parentTable.NewRow();
+            parentRow[0] = 1;
+            parentTable.Rows.Add(parentRow);
+
+            DataRow childRow = childTable.NewRow();
+            childRow[0] = 1;
+
+            //ошибка будет
+            //childRow[0] = 1;
+            //childTable.Rows.Add(childRow);
+
+        }
+    }
+}
---
+++ Project/App_ADO_Net/_004_SqlDataAdapter/_001_SqlDataAdapter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace App_ADO_Net._004_SqlDataAdapter
+{
+    public class _001_SqlDataAdapter
+    {
+        public _001_SqlDataAdapter()
+        {
+            string sqlConn =
+@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DB\Database1.mdf;Integrated Security=True;";
+
+            string commandStr = "Select * from Person";
+
+            DataTable dataTable = new DataTable();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(commandStr, sqlConn);
+
+            sqlDataAdapter.Fill(dataTable);
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (DataColumn col in dataTable.Columns)
+                {
+                    Console.WriteLine("{0} {1}", col.ColumnName, row[col]);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}

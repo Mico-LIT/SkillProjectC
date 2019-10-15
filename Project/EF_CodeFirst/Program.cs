eb320daa4e8c3ae2ed16387e6b78413a7bdc30be
Borovik Misha <redgame0@gmail.com>
2019-10-15 20:23:39 +0700

    EF CodeFirst Step 1

---
+++ Project/EF_CodeFirst/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_CodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new UserContext())
+            {
+                db.Users.Add(new Models.User.UserTMP()
+                {
+                    Age = 12,
+                    Name = "Jon"
+                });
+
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Project/EF_CodeFirst/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using EF_CodeFirst.Models.User;
+
+namespace EF_CodeFirst
+{
+    public class UserContext : DbContext
+    {
+        //public UserContext() : base("name=testDB")
+        //{ }
+
+        public DbSet<UserTMP> Users { get; set; }
+
+    }
+}

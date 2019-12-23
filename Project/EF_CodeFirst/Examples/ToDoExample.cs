c7d7da32cf932f926f4fa26a4b1a775df2ccc01b
Borovik Misha <redgame0@gmail.com>
2019-12-23 15:56:10 +0700

    EF - 'Linq to Entities'

---
+++ Project/EF_CodeFirst/Examples/ToDoExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_CodeFirst.Examples
+{
+    // TODO EF
+    /// <summary>
+    /// https://metanit.com/sharp/entityframework/3.13.php //Generic Repository
+    /// https://metanit.com/sharp/entityframework/3.12.php //Миграции
+    /// </summary>
+    class ToDoExample
+    {
+    }
+}
---
+++ Project/EF_CodeFirst/Examples/_005_LINQToEntities/_001_Linq.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_CodeFirst.Examples._005_LINQToEntities
+{
+    public class _001_Linq
+    {
+        public _001_Linq()
+        {
+            using (var db = new TrainintDBContext())
+            {
+                
+            }
+        }
+    }
+}

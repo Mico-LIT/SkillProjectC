af87c866b7c8821e5a9533bf040f86423ed25cf4
Borovik Misha <redgame0@gmail.com>
2020-03-08 23:37:46 +0700

    Updated Website

--- Project/WebApplication1/Controllers/Bootstrap4/PortfolioController.cs
+++ Project/WebApplication1/Controllers/Bootstrap4/PortfolioController.cs
@@ -9,15 +9,15 @@ namespace WebApplication1.Controllers.Bootstrap4
 {
     public class PortfolioController : Controller
     {
-        //https://medium.com/@stasonmars/%D1%83%D0%B7%D0%BD%D0%B0%D0%B5%D0%BC-bootstrap-4-%D0%B7%D0%B0-30-%D0%BC%D0%B8%D0%BD%D1%83%D1%82-%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D0%B2%D0%B0%D1%8F-%D0%BB%D0%B5%D0%BD%D0%B4%D0%B8%D0%BD%D0%B3-d268d52d6c84
-        // GET: Portfolio
+        //http://cvresumetemplate.com/maha-personal-cv-resume-html-template/home-two-w.html#mh-contact
         public ActionResult Index()
         {
             return View();
         }
 
-        //http://cvresumetemplate.com/maha-personal-cv-resume-html-template/home-two-w.html#mh-contact
-        public ActionResult Index2()
+        //https://medium.com/@stasonmars/%D1%83%D0%B7%D0%BD%D0%B0%D0%B5%D0%BC-bootstrap-4-%D0%B7%D0%B0-30-%D0%BC%D0%B8%D0%BD%D1%83%D1%82-%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D0%B2%D0%B0%D1%8F-%D0%BB%D0%B5%D0%BD%D0%B4%D0%B8%D0%BD%D0%B3-d268d52d6c84
+        // GET: Portfolio
+        public ActionResult Template()
         {
             return View();
         }

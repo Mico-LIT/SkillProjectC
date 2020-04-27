7f679926ac7ea760fe68ca350d14f70e933abee3
Borovik Misha <redgame0@gmail.com>
2020-04-27 19:29:32 +0700

    CSharp - Stack and Queue

---
+++ Project/CSharp/Base/StructureData/Queue/_001_Queue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Base.StructureData.Queue
+{
+    // Реализация на основе 2-х связного списка
+    class _001_Queue
+    {
+        class Queue<T>
+        {
+            LinkedList<T> _items = new LinkedList<T>();
+            public int Count => _items.Count;
+
+
+            public void Enqueue(T item)
+            {
+                _items.AddFirst(item);
+            }
+            public T Dequeue()
+            {
+                if (Count == 0)
+                    throw new InvalidOperationException("Queue empty");
+
+                var result = _items.Last.Value;
+                _items.RemoveLast();
+                return result;
+
+            }
+            public T Peek()
+            {
+                if (Count == 0)
+                    throw new InvalidOperationException("Queue empty");
+
+                return _items.Last.Value;
+            }
+        }
+
+
+        public _001_Queue()
+        {
+            Queue<int> instance = new Queue<int>();
+
+            instance.Enqueue(1);
+            instance.Enqueue(2);
+            instance.Enqueue(3);
+            instance.Enqueue(4);
+
+            Console.WriteLine("Count elements in Queue {0}", instance.Count);
+            Console.WriteLine("Peek {0}", instance.Peek());
+
+            var firstElement = instance.Dequeue();
+
+            Console.WriteLine("firstElement : {0}", firstElement);
+            Console.WriteLine("Count elements in Queue {0}", instance.Count);
+            Console.WriteLine("Peek : {0}", instance.Peek());
+        }
+    }
+}
---
+++ Project/CSharp/Base/StructureData/Stack/_001_Stack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Base.StructureData.Stack
+{
+    // Реализация на основе 2-х связного списка
+    class _001_Stack
+    {
+        class Stack<T>
+        {
+            LinkedList<T> _items = new LinkedList<T>();
+
+            public int Count => _items.Count;
+
+            public void Push(T item)
+            {
+                _items.AddLast(item);
+            }
+
+            public T Pop()
+            {
+                if (_items.Count == 0)
+                    throw new InvalidOperationException("Stack empty");
+
+                T result = _items.Last.Value;
+                _items.RemoveLast();
+
+                return result;
+            }
+
+            public T Peek()
+            {
+                return _items.Last.Value;
+            }
+
+        }
+
+
+        public _001_Stack()
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+
+            Console.WriteLine("Top stack {0}", stack.Peek());
+
+            var result = stack.Pop();
+            var result1 = stack.Pop();
+
+            Console.WriteLine();
+            Console.WriteLine("Top stack {0}", stack.Peek());
+            Console.WriteLine("Count {0}", stack.Count);
+
+        }
+    }
+}

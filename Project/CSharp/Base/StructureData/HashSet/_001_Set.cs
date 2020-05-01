17090fff566f3d77f061cf886b79057e45dac30a
Borovik Misha <redgame0@gmail.com>
2020-05-01 19:26:45 +0700

    CSharp - HashSet

---
+++ Project/CSharp/Base/StructureData/HashSet/_001_Set.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharp.Base.StructureData.HashSet
+{
+    class _001_Set
+    {
+        class Set<T> : IEnumerable<T> where T : IComparable<T>
+        {
+            List<T> _items = new List<T>();
+
+            public int Count => _items.Count;
+
+            public Set() { }
+
+            public Set(IEnumerable<T> items)
+            {
+                AddRange(items);
+            }
+
+            public void Remove(T item)
+            {
+                _items.Remove(item);
+            }
+
+            public bool Contains(T item)
+            {
+                return _items.Contains(item);
+            }
+
+            public void AddRange(IEnumerable<T> items)
+            {
+                foreach (T item in items)
+                {
+                    this.Add(item);
+                }
+            }
+
+            public void Add(T value)
+            {
+                if (Contains(value))
+                    throw new InvalidOperationException("Element in list exist  ");
+
+                _items.Add(value);
+            }
+
+
+            public IEnumerator<T> GetEnumerator()
+            {
+                return _items.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return _items.GetEnumerator();
+            }
+
+
+            // Объединение множеств
+            public Set<T> Union(Set<T> other)
+            {
+                var tmp = new Set<T>(_items);
+
+                foreach (var item in other._items)
+                {
+                    if (!tmp.Contains(item))
+                        tmp.Add(item);
+                }
+
+                return tmp;
+            }
+
+            // Разность множеств
+            public Set<T> Difference(Set<T> other)
+            {
+                var tmp = new Set<T>(_items);
+                foreach (var item in other._items)
+                    tmp.Remove(item);
+
+                return tmp;
+            }
+
+            // Пересечение множеств
+            public Set<T> Intersection(Set<T> other)
+            {
+                var tmp = new Set<T>();
+
+                foreach (var item in other._items)
+                {
+                    if (this._items.Contains(item))
+                        tmp.Add(item);
+                }
+
+                return tmp;
+            }
+
+            // Семметрическая разность множеств
+            public Set<T> SymmetricDifference(Set<T> other)
+            {
+                var intersect = Intersection(other);
+                var union = Union(other);
+                return union.Difference(intersect);
+            }
+
+        }
+
+        public _001_Set()
+        {
+            int[] arr = new int[] { 3, 4, 5, 6 };
+
+            Set<int> instance1 = new Set<int>();
+            Set<int> instance2 = new Set<int>(arr);
+            Set<int> instance3 = new Set<int>();
+
+            instance1.Add(1);
+            instance1.Add(2);
+            instance1.Add(3);
+            instance1.Add(4);
+
+            instance3 = instance1.SymmetricDifference(instance2);
+
+            foreach (var item in instance3)
+                Console.WriteLine(item);
+        }
+    }
+}
---
+++ Project/CSharp/Base/StructureData/HashSet/_002_HashSet.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Base.StructureData.HashSet
+{
+    class _002_HashSet
+    {
+        public _002_HashSet()
+        {
+            int[] array1 = { 1, 2, 3, 4, 5 };
+            int[] array2 = { 1, 3, 4, 7, 9 };
+
+            // Объединение множеств
+            Union(new HashSet<int>(array1), new HashSet<int>(array2));
+
+            // Разность множеств
+            Except(new HashSet<int>(array1), new HashSet<int>(array2));
+
+            // Пересечение множеств
+            Intersect(new HashSet<int>(array1), new HashSet<int>(array2));
+
+            // Симметрическая разность множеств
+            SymmetricExcept(new HashSet<int>(array1), new HashSet<int>(array2));
+
+            // Подмножество
+            IsSubset(new HashSet<int>(array1), new HashSet<int>(new int[] { 1, 2, 3, 4, 5, 6, 7 }));
+            //
+
+            int[] mass = new int[10];
+            var inst = new HashSet<int>(array1);
+            inst.CopyTo(mass);
+
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.WriteLine(mass[i]);
+            }
+
+            inst.Clear();
+        }
+
+        private void IsSubset(HashSet<int> hashSet1, HashSet<int> hashSet2)
+        {
+            show("set2:", hashSet1);
+            show("IsSubset set2:", hashSet2);
+            show($"= : {hashSet1.IsSubsetOf(hashSet2)}", Enumerable.Empty<int>());
+            Console.Write("\n\n");
+        }
+
+        private void SymmetricExcept(HashSet<int> hashSet1, HashSet<int> hashSet2)
+        {
+            show("set2:", hashSet1);
+            show("SymmetricExceptWith set2:", hashSet2);
+            hashSet1.SymmetricExceptWith(hashSet2);
+            show("= :", hashSet1);
+            Console.Write("\n\n");
+        }
+
+        private void Intersect(HashSet<int> hashSet1, HashSet<int> hashSet2)
+        {
+            show("set1:", hashSet1);
+            show("IntersectWith set2:", hashSet2);
+
+            hashSet1.IntersectWith(hashSet2);
+            show("= :", hashSet1);
+            Console.Write("\n\n");
+        }
+
+        private void Except(HashSet<int> hashSet1, HashSet<int> hashSet2)
+        {
+            show("set1:", hashSet1);
+            show("ExceptWith set2:", hashSet2);
+
+            hashSet1.ExceptWith(hashSet2);
+            show("= :", hashSet1);
+            Console.Write("\n\n");
+        }
+
+        private void Union(HashSet<int> hashSet1, HashSet<int> hashSet2)
+        {
+            show("set1:", hashSet1);
+            show("UnionWith set2:", hashSet2);
+
+            hashSet1.UnionWith(hashSet2);
+            show("= :", hashSet1);
+            Console.Write("\n\n");
+        }
+
+        private void show(string v, IEnumerable<int> hashSet)
+        {
+            Console.Write(" {0}\"{1}\" ", v, string.Join(",", hashSet));
+        }
+    }
+}

a4a30b4deb197f82cbda02fc160cd07a6ae0d577
Borovik Misha <Redgame0@gmail.com>
2020-11-09 15:06:51 +0700

    CSharp - Algorithms.BubbleSort v2

--- Project/CSharp/Base/Algorithms/BubbleSort.cs
+++ Project/CSharp/Base/Algorithms/BubbleSort.cs
@@ -14,13 +14,52 @@ namespace CSharp.Base.Algorithms
             Display(arr);
 
             Sort(arr);
-
             Display(arr);
+            //Display(arr, true);
 
-            Display(arr, true);
+
+            int[] arr2 = { 5, 4, 3, 2, 1, 77, 3, 11 };
+            Sort2(ref arr2);
+            Display(arr2);
 
             Console.ReadLine();
         }
+
+
+        void Sort2(ref int[] arr)
+        {
+            bool swapped;
+            // To improve
+            int j = 0;
+
+            do
+            {
+                swapped = false;
+
+                for (int i = 1; i < arr.Length - j; i++)
+                {
+                    if (arr[i - 1].CompareTo(arr[i]) > 0)
+                    {
+                        swap(ref arr, i - 1, i);
+                        swapped = true;
+                    }
+                }
+
+                j++;
+            } while (swapped != false);
+
+        }
+
+        private void swap(ref int[] arr, int left, int right)
+        {
+            if (left != right)
+            {
+                var tmp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = tmp;
+            }
+        }
+
 
         // Сложность = O(n^2)
         void Sort(int[] arr)

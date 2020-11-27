81142562028f30bf2bfca02b9a8fdff3b22b372f
Borovik Misha <Redgame0@gmail.com>
2020-11-27 20:58:23 +0700

    CSharp - StructureData.HashTable

---
+++ Project/CSharp/Base/StructureData/HashTable/_001_HashTableNodePair.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Base.StructureData.HashTable
+{
+    public class _001_HashTableNodePair<TKey, TValue>
+    {
+        public TKey Key { get; private set; }
+        public TValue Value { get; private set; }
+
+        public _001_HashTableNodePair(TKey key, TValue value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+    }
+}
---
+++ Project/CSharp/Base/StructureData/HashTable/_002_HashTableArrayNode.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Base.StructureData.HashTable
+{
+    public class _002_HashTableArrayNode<Tkey, TValue>
+    {
+        LinkedList<_001_HashTableNodePair<Tkey, TValue>> _items;
+
+        public void Add(Tkey key, TValue value)
+        {
+            if (_items == null)
+            {
+                _items = new LinkedList<_001_HashTableNodePair<Tkey, TValue>>();
+            }
+            else
+            {
+                foreach (var item in _items)
+                {
+                    if (item.Key.Equals(key))
+                        throw new InvalidOperationException("Key is exist");
+                }
+            }
+
+            _items.AddLast(new _001_HashTableNodePair<Tkey, TValue>(key, value));
+        }
+
+        public System.Collections.Generic.IEnumerator<Tkey> GetEnumerator()
+        {
+            if (_items != null)
+            {
+                foreach (_001_HashTableNodePair<Tkey, TValue> node in _items)
+                    yield return node.Key;
+            }
+        }
+
+
+        public static void Test()
+        {
+            var instanse = new _002_HashTableArrayNode<string, string>();
+
+            instanse.Add("1", "2");
+            instanse.Add("2", "4");
+            instanse.Add("3", "3");
+
+            foreach (var item in instanse)
+                Console.WriteLine(item);
+
+        }
+    }
+}

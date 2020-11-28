438b0986e6356ec3df757e5be61ce62ab0af4b2c
Borovik Misha <Redgame0@gmail.com>
2020-11-28 13:15:07 +0700

    CSharp - StructureData.HashTable step 2

--- Project/CSharp/Base/StructureData/HashTable/_001_HashTableNodePair.cs
+++ Project/CSharp/Base/StructureData/HashTable/_001_HashTableNodePair.cs
@@ -9,7 +9,7 @@ namespace CSharp.Base.StructureData.HashTable
     public class _001_HashTableNodePair<TKey, TValue>
     {
         public TKey Key { get; private set; }
-        public TValue Value { get; private set; }
+        public TValue Value { get; set; }
 
         public _001_HashTableNodePair(TKey key, TValue value)
         {
--- Project/CSharp/Base/StructureData/HashTable/_002_HashTableArrayNode.cs
+++ Project/CSharp/Base/StructureData/HashTable/_002_HashTableArrayNode.cs
@@ -6,49 +6,93 @@ using System.Threading.Tasks;
 
 namespace CSharp.Base.StructureData.HashTable
 {
-    public class _002_HashTableArrayNode<Tkey, TValue>
+    public class _002_HashTableArrayNode
     {
-        LinkedList<_001_HashTableNodePair<Tkey, TValue>> _items;
-
-        public void Add(Tkey key, TValue value)
+        public _002_HashTableArrayNode()
         {
-            if (_items == null)
-            {
-                _items = new LinkedList<_001_HashTableNodePair<Tkey, TValue>>();
-            }
-            else
-            {
-                foreach (var item in _items)
-                {
-                    if (item.Key.Equals(key))
-                        throw new InvalidOperationException("Key is exist");
-                }
-            }
-
-            _items.AddLast(new _001_HashTableNodePair<Tkey, TValue>(key, value));
-        }
-
-        public System.Collections.Generic.IEnumerator<Tkey> GetEnumerator()
-        {
-            if (_items != null)
-            {
-                foreach (_001_HashTableNodePair<Tkey, TValue> node in _items)
-                    yield return node.Key;
-            }
-        }
-
-
-        public static void Test()
-        {
-            var instanse = new _002_HashTableArrayNode<string, string>();
+            var instanse = new HashTableArrayNode<string, string>();
 
             instanse.Add("1", "2");
             instanse.Add("2", "4");
             instanse.Add("3", "3");
 
+            instanse.Update("22", "22");
+
+            if (instanse.TryGetValue("2", out var result))            
+                Console.WriteLine($"result: {result}");
+            
+
             foreach (var item in instanse)
                 Console.WriteLine(item);
+        }
+
+        class HashTableArrayNode<TKey, TValue>
+        {
+            LinkedList<_001_HashTableNodePair<TKey, TValue>> _items;
+
+            public void Add(TKey key, TValue value)
+            {
+                if (_items == null)
+                {
+                    _items = new LinkedList<_001_HashTableNodePair<TKey, TValue>>();
+                }
+                else
+                {
+                    foreach (var item in _items)
+                    {
+                        if (item.Key.Equals(key))
+                            throw new ArgumentException("Key is exist");
+                    }
+                }
+
+                _items.AddLast(new _001_HashTableNodePair<TKey, TValue>(key, value));
+            }
+
+            public void Update(TKey key, TValue value)
+            {
+
+                if (_items != null)
+                {
+                    foreach (_001_HashTableNodePair<TKey, TValue> item in _items)
+                        if (item.Key.Equals(key))
+                        {
+                            item.Value = value;
+                            return;
+                        }
+
+                    throw new ArgumentException("Key is exist");
+                }
+            }
+
+            public bool TryGetValue(TKey key, out TValue value)
+            {
+                value = default;
+                bool found = false;
+
+                if (_items != null)
+                {
+                    foreach (_001_HashTableNodePair<TKey, TValue> pair in _items)
+                        if (pair.Key.Equals(key))
+                        {
+                            value = pair.Value;
+                            found = true;
+                            break;
+                        }
+                }
+
+                return found;
+            }
+
+            public System.Collections.Generic.IEnumerator<TKey> GetEnumerator()
+            {
+                if (_items != null)
+                {
+                    foreach (_001_HashTableNodePair<TKey, TValue> node in _items)
+                        yield return node.Key;
+                }
+            }
 
         }
+
     }
 }

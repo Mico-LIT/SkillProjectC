f02a6333b4ab73e81c5931b06319c4727f2992f4
Borovik Misha <Redgame0@gmail.com>
2021-01-06 12:23:25 +0700

    CSharp - UniversalTemplate step 3

--- Project/CSharp/Base/UniversalTemplate/Collections/_005_LinkedList.cs
+++ Project/CSharp/Base/UniversalTemplate/Collections/_005_LinkedList.cs
@@ -10,6 +10,22 @@ namespace CSharp.Base.UniversalTemplate.Collections
     {
         public _005_LinkedList()
         {
+            LinkedList<int> vs = new LinkedList<int>();
+
+            vs.AddFirst(1);
+            vs.AddFirst(2);
+            var first = vs.First;
+
+            vs.AddBefore(first, 4);
+            vs.AddAfter(first, 3);
+
+            var node = vs.First;
+
+            while (node !=null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
         }
     }
 }

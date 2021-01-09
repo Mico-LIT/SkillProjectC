55acd50632612f2cd4f6f0b1eb168611e96b29d9
Borovik Misha <Redgame0@gmail.com>
2021-01-09 19:16:23 +0700

    CSharp - UniversalTemplate step 5

--- Project/CSharp/Base/UniversalTemplate/Collections/_006_Dictionary.cs
+++ Project/CSharp/Base/UniversalTemplate/Collections/_006_Dictionary.cs
@@ -8,5 +8,18 @@ namespace CSharp.Base.UniversalTemplate.Collections
 {
     class _006_Dictionary
     {
+        public _006_Dictionary()
+        {
+            Dictionary<int, EmployeeModel> emloyeeDict = new Dictionary<int, EmployeeModel>();
+
+            emloyeeDict.Add(2, new EmployeeModel() { Name = "Andre" });
+            emloyeeDict.Add(1, new EmployeeModel() { Name = "Misha" });
+           // emloyeeDict.Add(1, new EmployeeModel() { Name = "Andre" });  // Error
+
+            foreach (var item in emloyeeDict)
+            {
+                Console.WriteLine("Key:{0} Value.Name:{1}", item.Key, item.Value.Name);
+            }
+        }
     }
 }
--- Project/CSharp/Base/UniversalTemplate/Collections/_007_SortedSet.cs
+++ Project/CSharp/Base/UniversalTemplate/Collections/_007_SortedSet.cs
@@ -8,5 +8,16 @@ namespace CSharp.Base.UniversalTemplate.Collections
 {
     class _007_SortedSet
     {
+        public _007_SortedSet()
+        {
+            // тоже самое что и HashSet только все множество в упорядоченом виде
+            SortedSet<int> vs = new SortedSet<int> { 1, 4, 3, 6, 7, 7, 3, 21, 1, 22, };
+
+            foreach (var item in vs)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
     }
 }

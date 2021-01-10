345139b420106a8c55b832954f8189971c287897
Borovik Misha <Redgame0@gmail.com>
2021-01-10 14:10:13 +0700

    CSharp - UniversalTemplate step 6

--- Project/CSharp/Base/UniversalTemplate/Collections/_008_SortedList.cs
+++ Project/CSharp/Base/UniversalTemplate/Collections/_008_SortedList.cs
@@ -8,5 +8,20 @@ namespace CSharp.Base.UniversalTemplate.Collections
 {
     class _008_SortedList
     {
+        public _008_SortedList()
+        {    
+            // Единственное отличие от Dictinary это SortedList использует меньше памяти
+            SortedList<int, string> keyValuePairs = new SortedList<int, string>();
+
+            //keyValuePairs.Add(2, "trr");
+            keyValuePairs.Add(2, "trr");
+            keyValuePairs.Add(4, "trr1");
+            keyValuePairs.Add(1, "trr2");
+
+            foreach (var item in keyValuePairs)
+            {
+                Console.WriteLine("Key:{0} Value:{1}", item.Key, item.Value);
+            }
+        }
     }
 }
--- Project/CSharp/Base/UniversalTemplate/Collections/_009_SortedDictionary.cs
+++ Project/CSharp/Base/UniversalTemplate/Collections/_009_SortedDictionary.cs
@@ -8,5 +8,17 @@ namespace CSharp.Base.UniversalTemplate.Collections
 {
     class _009_SortedDictionary
     {
+        public _009_SortedDictionary()
+        {    
+            // sorted by key
+            SortedDictionary<int, EmployeeModel> keyValuePairs = new SortedDictionary<int, EmployeeModel>();
+            keyValuePairs.Add(2, new EmployeeModel() { Name = "Andre" });
+            keyValuePairs.Add(1, new EmployeeModel() { Name = "Misha" });
+
+            foreach (var item in keyValuePairs)
+            {
+                Console.WriteLine("Key:{0} Value.Name:{1}", item.Key, item.Value.Name);
+            }
+        }
     }
 }

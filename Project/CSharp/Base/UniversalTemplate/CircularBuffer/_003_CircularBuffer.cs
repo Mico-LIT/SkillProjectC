db471124f290b82f9cd5d7ce97cff75ca5e679fd
Borovik Misha <Redgame0@gmail.com>
2021-01-12 11:23:07 +0700

    CSharp - UniversalTemplate step 8

---
+++ Project/CSharp/Base/UniversalTemplate/CircularBuffer/_003_CircularBuffer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Base.UniversalTemplate.CircularBuffer
+{
+    class _003_CircularBuffer<T> : _002_Buffer<T>
+    {
+        int capacity;
+
+        public bool IsFull => items.Count == capacity;
+
+        public _003_CircularBuffer() : this(10)
+        {
+        }
+
+        public _003_CircularBuffer(int count)
+        {
+            this.capacity = count;
+        }
+
+        public void Write(T value)
+        {
+            Write(value);
+
+            if (items.Count > capacity)            
+                items.Dequeue();
+            
+        }
+    }
+}

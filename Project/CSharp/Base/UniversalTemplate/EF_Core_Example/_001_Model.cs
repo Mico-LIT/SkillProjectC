97f56fba457fc0ce84d7f079888f3b65c2ba8c08
Borovik Misha <Redgame0@gmail.com>
2021-01-18 14:38:08 +0700

    CSharp - UniversalTemplate step 13 EF Core

---
+++ Project/CSharp/Base/UniversalTemplate/EF_Core_Example/_001_Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Base.UniversalTemplate.EF_Core_Example
+{
+    public class _001_Model
+    {
+        public class Employee
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+
+
+    }
+}
---
+++ Project/EF_Core_UniversalTemplate/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using CSharp.Base.UniversalTemplate.EF_Core_Example;
+
+namespace EF_Core_UniversalTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        class EmployeeDb : DbContext
+        {
+            public DbSet<_001_Model.Employee> Employees { get; set; }
+        }
+    }
+}

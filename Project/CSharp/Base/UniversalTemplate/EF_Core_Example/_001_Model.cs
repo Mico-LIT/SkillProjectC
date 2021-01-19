22dc8b41790f3408ce0e5a7b5bf4bcbbb8dded21
Borovik Misha <Redgame0@gmail.com>
2021-01-19 11:46:46 +0700

    CSharp - UniversalTemplate step 14 EF Core models

--- Project/CSharp/Base/UniversalTemplate/EF_Core_Example/_001_Model.cs
+++ Project/CSharp/Base/UniversalTemplate/EF_Core_Example/_001_Model.cs
@@ -8,13 +8,31 @@ namespace CSharp.Base.UniversalTemplate.EF_Core_Example
 {
     public class _001_Model
     {
-        public class Employee
+        public interface IEntity
+        {
+            int Id { get; set; }
+            bool IsValid();
+        }
+
+        public class Employee : IEntity
         {
             public int Id { get; set; }
             public string Name { get; set; }
+
+            public bool IsValid()
+            {
+                return true;
+            }
         }
 
 
-
+        public interface IRepository<T> : IDisposable
+        {
+            void Add(T item);
+            void Deleted(T item);
+            T FindById(int id);
+            IQueryable<T> FindAll();
+            int Commit();
+        }
     }
 }

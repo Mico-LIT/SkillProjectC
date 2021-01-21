130dbc119590d26d92b2135c68a007610720305c
Borovik Misha <Redgame0@gmail.com>
2021-01-21 15:02:54 +0700

    CSharp - UniversalTemplate step 16 EF Core

---
+++ Project/EF_Core_UniversalTemplate/DataBase/CompanyDb.cs
@@ -0,0 +1,24 @@
+﻿using CSharp.Base.UniversalTemplate.EF_Core_Example;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EF_Core_UniversalTemplate.DataBase
+{
+    class CompanyDb : DbContext
+    {
+        public DbSet<_001_Model.Employee> Employees { get; set; }
+
+        public CompanyDb()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("Test");
+            //base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Project/EF_Core_UniversalTemplate/DataBase/SqlRepository.cs
@@ -0,0 +1,63 @@
+﻿using CSharp.Base.UniversalTemplate.EF_Core_Example;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EF_Core_UniversalTemplate.DataBase
+{
+    // Пример:
+    //public class SqlRepository<T, T2, T3>    where T : class, _001_Model.IEntity, new()
+    //                                         where T2 : class, IDisposable, IFormattable, new()
+    //                                         where T3 : struct, IQueryable<T3>, IEquatable<T3>
+    //{ }
+
+    public class SqlRepository<T> : _001_Model.IRepository<T> where T : class, _001_Model.IEntity, new()
+    {
+        DbContext dbContext;
+        DbSet<T> dbSet;
+
+        public SqlRepository(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            dbSet = dbContext.Set<T>();
+        }
+
+        public void Add(T item)
+        {
+            if (item.IsValid())
+            {
+                dbSet.Add(item);
+            }
+            else
+                throw new InvalidOperationException();
+
+        }
+
+        public int Commit()
+        {
+            return dbContext.SaveChanges();
+        }
+
+        public void Deleted(T item)
+        {
+            dbSet.Remove(item);
+        }
+
+        public void Dispose()
+        {
+            dbContext.Dispose();
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            return dbSet;
+        }
+
+        public T FindById(int id)
+        {
+            return dbSet.Find(id);
+        }
+    }
+}

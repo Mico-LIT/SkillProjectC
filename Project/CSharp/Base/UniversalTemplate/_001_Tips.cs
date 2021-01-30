60128dfba2115e79d58f9c4cdce5be375fec4988
Borovik Misha <Redgame0@gmail.com>
2021-01-30 14:27:20 +0700

    CSharp - UniversalTemplate step 23 Reflection and MsTest

---
+++ Project/CSharp/Base/UniversalTemplate/_001_Tips.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Base.UniversalTemplate
+{
+    class _001_Tips
+    {
+        public _001_Tips()
+        {
+            var list = new List<Item>();
+            list.Add(new Item<int>());
+            list.Add(new Item<double>());
+        }
+
+        public class Item<T> : Item 
+        { }
+
+        public class Item 
+        { }
+    }
+}
---
+++ Project/CSharp/Base/UniversalTemplate/_002_Tips.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Base.UniversalTemplate
+{
+    class _002_Tips
+    {
+        public _002_Tips()
+        {
+            var a = new Item<int>();
+            var d = new Item<int>();
+            var c = new Item<double>();
+
+            Console.WriteLine(Item<int>.InstanceCount);   // diff InstanceCount
+            Console.WriteLine(Item<double>.InstanceCount);// diff InstanceCount
+        }
+
+        public class Item<T>
+        {
+            public static int InstanceCount;
+
+            public Item()
+            {
+                InstanceCount++;
+            }
+        }
+
+        // InstanceCount будет одинаковый
+
+        //public class Item<T> : Item
+        //{
+        //}
+
+        //public class Item
+        //{
+        //    public static int InstanceCount;
+
+        //    public Item()
+        //    {
+        //        InstanceCount++;
+        //    }
+        //}
+    }
+}

810785a48a4b9a61c9d403a9194547bb7437c663
Borovik Misha <Redgame0@gmail.com>
2021-02-09 22:13:50 +0700

    UnitTesting - PasswordStrength

---
+++ Project/UnitTesting/Code.Tests/PasswordCheckTests.cs
@@ -0,0 +1,28 @@
+﻿using Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Tests
+{
+    [TestClass]
+    public class PasswordCheckTests
+    {
+        [TestMethod()]
+        public void GetPasswordStrength_c_result5Ballov()
+        {
+            //Arrange
+            string pass = "Pasv123!";
+            int result = 5;
+            //Act
+            int strengthPass = new PasswordCheck().GetPasswordStrength(pass);
+
+
+            //Assert
+            Assert.AreEqual(result, strengthPass);
+        }
+    }
+}
---
+++ Project/UnitTesting/Code/PasswordCheck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    public class PasswordCheck
+    {
+        public int GetPasswordStrength(string password)
+        {
+
+            if (string.IsNullOrWhiteSpace(password))
+                return 0;
+
+            int conutStrength = 0;
+
+            if (Math.Max(password.Length, 7) > 7)
+                conutStrength++;
+
+            if (Regex.Match(password, "[a-z]").Success)
+                conutStrength++;
+            if (Regex.Match(password, "[A-Z]").Success)
+                conutStrength++;
+            if (Regex.Match(password, "[0-9]").Success)
+                conutStrength++;
+            if (Regex.Match(password, @"[\\!\\@\\#\\$]").Success)
+                conutStrength++;
+
+            return conutStrength;
+        }
+    }
+}

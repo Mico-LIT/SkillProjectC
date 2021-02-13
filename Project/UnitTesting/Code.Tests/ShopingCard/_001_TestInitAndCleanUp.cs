f122d16a574d448e580dfdb1aab3ea10569c577a
Borovik Misha <Redgame0@gmail.com>
2021-02-13 11:38:45 +0700

    UnitTesting - ShopingCard

---
+++ Project/UnitTesting/Code.Tests/ShopingCard/_001_TestInitAndCleanUp.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Tests.ShopingCard
+{
+    [TestClass]
+    public class _001_TestInitAndCleanUp
+    {
+        _001_ShopingCard card;
+        _001_ShopingCard.Item item;
+
+        //Запускается перед стартом каждого тестирущего метода
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Debug.WriteLine("TestInitialize");
+
+            item = new _001_ShopingCard.Item();
+            item.Name = "Page";
+            item.Quantity = 2;
+
+            card = new _001_ShopingCard();
+            card.Add(item);
+        }
+
+        //Запускается после каждого тестирущего метода
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            Debug.WriteLine("TestCleanUp");
+            card.Dispose();
+        }
+
+        [TestMethod]
+        public void ShopingCard_CheckOut_ContainsIten()
+        {
+            CollectionAssert.Contains(card.Items, item);
+        }
+
+        [TestMethod]
+        public void ShopingCard_RemoveItems_Empty()
+        {
+            //Arrange
+            int expected = 0;
+            //Act
+            card.Removed(0);
+            //Assert
+            Assert.AreEqual(expected, card.Count);
+        }
+    }
+}
---
+++ Project/UnitTesting/Code/_001_ShopingCard.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    [Serializable]
+    public class _001_ShopingCard : IDisposable
+    {
+        public List<Item> Items = new List<Item>();
+
+        public int Count => Items.Count;
+
+        public void Add(Item item)
+        {
+            Items.Add(item);
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Removed(int index)
+        {
+            Items.RemoveAt(index);
+        }
+
+        [Serializable]
+        public class Item
+        {
+            public string Name { get; set; }
+            public int Quantity { get; set; }
+
+        }
+    }
+
+}

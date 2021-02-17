3e9ccf5d7d6f5f0adab181c5a863076808c26083
Borovik Misha <Redgame0@gmail.com>
2021-02-17 11:49:52 +0700

    UnitTesting - StringAssert Step 5

---
+++ Project/UnitTesting/Code.Tests/_03_StringAssert/StringAssertMethods.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Tests._03_StringAssert
+{
+    [TestClass]
+    public class StringAssertMethods
+    {
+        [TestMethod]
+        public void ContainsTest()
+        {
+            StringAssert.Contains("Hello misha", "is");
+        }
+
+        [TestMethod]
+        public void MatchesTest()
+        {
+            StringAssert.Matches("gfd1", new System.Text.RegularExpressions.Regex(@"\d{1}"));
+        }
+
+        [TestMethod]
+        public void EndsWithTest()
+        {
+            StringAssert.EndsWith("Hello misha", "misha");
+        }
+
+        [TestMethod]
+        public void StartsWithTest()
+        {
+            StringAssert.StartsWith("hello misha", "hello");
+        }
+    }
+}

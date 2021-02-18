61f15b865ac69095569736522ac13e91eaab42b9
Borovik Misha <Redgame0@gmail.com>
2021-02-18 14:57:43 +0700

    UnitTesting - Expecting Exception Step 7

---
+++ Project/UnitTesting/Code.Tests/_04_ExpectingExceptions/ExpectingExceptionsMethods.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Tests._04_ExpectingExceptions
+{
+    [TestClass]
+    public class ExpectingExceptionsMethods
+    {
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "Exception not throw")]
+        public void Example_SeyHello_returnException()
+        {
+            new _003_ExampleAssert().SeyHello(null);
+        }
+
+        [TestMethod]
+        public void Example_SeyHello_FullString()
+        {
+            // Arrange
+            string expected = "Hello misha";
+            string input = "misha";
+
+            // Act
+            var result = new _003_ExampleAssert().SeyHello(input);
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+
+    }
+}
---
+++ Project/UnitTesting/Code.Tests/_05_TestContext/TestContextMethods.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Tests._05_TestContext
+{
+    [TestClass]
+    public class TestContextMethods
+    {
+        // TestContext для хранении информации о текущем юник тесте 
+        // При тестирование web services хранит URL
+        // При тестировании ASP.NET приложений - передоставляет доступ к ASP страницам
+        // При исполизовании Data Driven тестов предоставляет доступ источнику данных
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TestContext.WriteLine("TestRunDirectory {0}", TestContext.TestRunDirectory);
+            TestContext.WriteLine("TestName {0}", TestContext.TestName);
+            TestContext.WriteLine("CurrentTestOutcome {0}", TestContext.CurrentTestOutcome);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestContext.WriteLine("(CleanUp) {0}", TestContext.TestName);
+            TestContext.WriteLine("(CleanUp) {0}", TestContext.CurrentTestOutcome);
+        }
+    }
+}

d09f09f5d4d9fc3d767d839f8a842217a53114e3
Borovik Misha <Redgame0@gmail.com>
2021-02-23 12:16:29 +0700

    UnitTesting - InternalClassTest step 10

---
+++ Project/UnitTesting/Code.Tests/_07_InternalClass/_01_ExampleTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Tests._07_InternalClass
+{
+    [TestClass]
+    public class _01_ExampleTest
+    {
+        [TestMethod]
+        public void TestExample()
+        {
+            var example = new InternalClassTest._001_Example();
+            bool result = example.Tmp();
+
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ Project/UnitTesting/Code/InternalClassTest/_001_Example.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+// InternalsVisibleTo - указывает, что типы, которые обычно доступны только в текущей сборке будут доступны также в сборке,
+// которая определена в параметрах.
+[assembly: InternalsVisibleTo("Code.Tests")]
+
+namespace Code.InternalClassTest
+{
+    // по умолчанию типа доступа Internal (Доступна в текущей сборки)
+    class _001_Example
+    {
+        public bool Tmp()
+        {
+            return true;
+        }
+    }
+}

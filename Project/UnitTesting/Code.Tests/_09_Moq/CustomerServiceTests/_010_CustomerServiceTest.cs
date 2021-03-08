7b3f9e4735a805d2cd802d75f361ce44aad4c1e0
Borovik Misha <Redgame0@gmail.com>
2021-03-08 11:11:02 +0700

    UnitTesting - Moq CustomerService ( MockingProperty ) Step 10

---
+++ Project/UnitTesting/Code.Tests/_09_Moq/CustomerServiceTests/_010_CustomerServiceTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Code.Moq._010_CustomerService.Classes;
+using Code.Moq._010_CustomerService.Interfaces;
+using Code.Moq._010_CustomerService;
+
+namespace Code.Tests._09_Moq.CustomerServiceTests
+{
+    /// <summary>
+    /// MockingProperty
+    /// </summary>
+    [TestClass]
+    public class _010_CustomerServiceTest
+    {
+        [TestMethod]
+        public void Create_ShouldSetTimeZone_CallSave()
+        {
+            // Aggregate
+            var moqCustomerRepository = new Mock<ICustomerRepository>();
+            var moqWorkSettings = new Mock<IWorkSettings>();
+
+            moqWorkSettings.Setup(x => x.WorkId).Returns(12);
+
+            CustomerService customerService = new CustomerService(moqWorkSettings.Object, moqCustomerRepository.Object);
+
+            // Act
+            customerService.Create(new CustomerDTO());
+
+            moqCustomerRepository.VerifySet(x => x.LolacTimeZone = It.IsAny<string>());
+            moqCustomerRepository.Verify(x => x.Save(It.IsAny<Customer>()));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Create_ShouldSetWorkId_ArgumentNullException()
+        {
+            // Aggregate
+            var moqCustomerRepository = new Mock<ICustomerRepository>();
+            var moqWorkSettings = new Mock<IWorkSettings>();
+
+            moqWorkSettings.Setup(x => x.WorkId).Returns((int?)null);
+
+            CustomerService customerService = new CustomerService(moqWorkSettings.Object, moqCustomerRepository.Object);
+
+            // Act
+            customerService.Create(new CustomerDTO());
+        }
+    }
+}
---
+++ Project/UnitTesting/Code/Moq/_010_CustomerService/CustomerService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Code.Moq._010_CustomerService.Classes;
+using Code.Moq._010_CustomerService.Interfaces;
+
+namespace Code.Moq._010_CustomerService
+{
+    public class CustomerService
+    {
+        private readonly ICustomerRepository customerRepository;
+        private readonly IWorkSettings workSettings;
+
+        public CustomerService(IWorkSettings workSettings, ICustomerRepository customerRepository)
+        {
+            this.workSettings = workSettings;
+            this.customerRepository = customerRepository;
+        }
+
+        public void Create(CustomerDTO customerDTO)
+        {
+            Customer newCustomer = new Customer();
+
+            newCustomer.Id = Guid.NewGuid();
+            newCustomer.FirstName = customerDTO.FirstName;
+            newCustomer.LastName = customerDTO.LastName;
+
+            int? workId = workSettings.WorkId;
+
+            if (!workId.HasValue)            
+                throw new ArgumentNullException();
+
+            newCustomer.WorkId = workId.Value;
+
+            // Тест должен проверить что свойство установлено
+            customerRepository.LolacTimeZone = TimeZoneInfo.Local.DisplayName;
+
+            customerRepository.Save(newCustomer);
+        }
+    }
+}

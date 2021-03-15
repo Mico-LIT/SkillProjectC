40b6a1c11497f5bbd9500d52e62b2197556ac7d0
dgdf <fgdf>
2021-03-15 20:27:36 +0700

    ASP - MiddelWare and ILoggerFactory

---
+++ Project/Web/ASP/_002_Example/CustomMiddelware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _002_Example
+{
+    public class CustomMiddelware
+    {
+        // Ссылка на след компонент конвеера запросов
+        readonly RequestDelegate next;
+        private readonly ILogger logger;
+
+        public CustomMiddelware(RequestDelegate next, ILogger<CustomMiddelware> logger)
+        {
+            this.next = next;
+            this.logger = logger;
+        }
+
+        // Должен быть Invoke или InvokeAsync!
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var methodName = context.Request.Method;
+            if (methodName == "GET")
+            {
+                logger.LogWarning("CustomMiddelware");
+
+                context.Response.StatusCode = StatusCodes.Status200OK;
+                await context.Response.WriteAsync("HI CustomMiddelware");
+            }
+            else
+            {
+                // передаем дальше по конвееру запросов
+                await next(context);
+            }
+        }
+    }
+}
---
+++ Project/Web/ASP/_002_Example/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _002_Example
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Middleware
+            app.UseMiddleware<CustomMiddelware>();
+
+            // Можно поменять (Конвеер обработки запросов) порядок и разрешить доступ к файлам без аутентификации!
+
+            // Аутентификация пользователя на основе куки
+            //app.UseAuthentication();
+            // Установка обработчика статических файлов
+            app.UseStaticFiles();
+
+            // Логика обработки запроса
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hi");
+            });
+
+        }
+    }
+}

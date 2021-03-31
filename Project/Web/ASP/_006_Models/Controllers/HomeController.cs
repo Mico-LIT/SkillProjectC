b532fb0f8d3439ca2f9bbc961ceb344d297c06b6
Borovik Misha <redgame0@gmail.com>
2021-03-31 21:35:29 +0700

    ASP - Models step 2

--- Project/Web/ASP/_006_Models/Controllers/HomeController.cs
+++ Project/Web/ASP/_006_Models/Controllers/HomeController.cs
@@ -29,11 +29,36 @@ namespace _006_Models.Controllers
 
             return $"Id {company.Id} Name {company.Name}";
         }
+
         public string _002_FatModel()
         {
             Models._002_FatModel.Company company = new Models._002_FatModel.Company(12312312, "Microsoft");
 
             return company.GetInfo();
+        }
+
+        public IActionResult _003_SingleModel()
+        {
+            Models._003_SinglModel.Game game = new Models._003_SinglModel.Game()
+            {
+                Name = "Pank",
+                Platform = "PC",
+                Engine = "Unity"
+            };
+
+            return View(game);
+        }
+
+        public IActionResult _003_Models()
+        {
+            List<Models._003_SinglModel.Game> game = new List<Models._003_SinglModel.Game>()
+            {
+                new Models._003_SinglModel.Game(){Name = "1" ,Platform = "PC" , Engine = "11"},
+                new Models._003_SinglModel.Game(){Name = "2" ,Platform = "Xbox" , Engine = "22"},
+                new Models._003_SinglModel.Game(){Name = "3" ,Platform = "psp" , Engine = "33"},
+            };
+
+            return View(game);
         }
     }
 }

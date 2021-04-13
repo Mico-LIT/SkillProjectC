65fa9d0bd6679c72bd0af4b8198d114f3b07f4e8
Borovik Misha <redgame0@gmail.com>
2021-04-13 12:15:32 +0700

    ASP - WebAPI step 2 "File result and static file"

--- Project/Web/ASP/_007_WebAPI/Pages/Index.cshtml.cs
+++ Project/Web/ASP/_007_WebAPI/Pages/Index.cshtml.cs
@@ -23,5 +23,19 @@ namespace _007_WebAPI.Pages
         //public ContentResult OnGet() { return Content("HI"); }
         //public ForbidResult OnGet() { return Forbid(); }
         //public NotFoundResult OnGet() { return NotFound(); }
+
+        //public VirtualFileResult OnGet()
+        //{
+        //    var path = Path.Combine("~/", "HiPerson.html");
+        //    var bytes = System.IO.File.ReadAllBytes(path);
+        //    return File(path, "application/html", "Test.html");
+        //}
+
+        //public FileResult OnGet()
+        //{
+        //    var path = Path.Combine(HostEnvironment.ContentRootPath, "wwwroot/HiPerson.html");
+        //    var bytes = System.IO.File.ReadAllBytes(path);
+        //    return File(bytes, "application/html", "Test.html");
+        //}
     }
 }

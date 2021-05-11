2e1522aa1609ddd6e3f196f5fa3a9d4f92db3103
Borovik Misha <redgame0@gmail.com>
2021-05-11 11:30:47 +0700

    ASP - My site

--- Project/Web/ASP/imickey/Controllers/HomeController.cs
+++ Project/Web/ASP/imickey/Controllers/HomeController.cs
@@ -10,21 +10,11 @@ namespace imickey.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Index1()
-        {
             var dateStart = new DateTime(2016, 11, 1);
             double digital = Math.Floor(DateTime.UtcNow.Subtract(dateStart).TotalDays / 365);
             ViewBag.DateWork = digital;
 
             return View();
-        }
-
-        public string Error()
-        {
-            return "Error";
         }
     }
 }
---
+++ Project/Web/ASP/imickey/Controllers/OtherController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace imickey.Controllers
+{
+    public class OtherController : Controller
+    {
+        public IActionResult FirstVersionMySite()
+        {
+            return View();
+        }
+
+
+        public string Error()
+        {
+            return "Error";
+        }
+    }
+}
